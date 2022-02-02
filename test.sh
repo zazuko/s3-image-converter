c9c988e9281aad47c754ac7caac8970d308e1e48
Ludovic Muller <ludovicmuller1@gmail.com>
2022-02-02 13:53:18 +0100

    test: initial setup

---
+++ test.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+echo "Test the Docker image"
+
+# display steps with date and time
+print_step() {
+  CURRENT_DATE=$(date +"%m-%d-%Y %T")
+  echo "\n\n[${CURRENT_DATE}] $1"
+}
+
+print_step "Build and pull images"
+docker-compose pull
+docker-compose build
+
+print_step "Start the stack"
+docker-compose up -d
+
+print_step "Wait that the stack is ready"
+RETRIES=60
+while true; do
+  curl -sL http://localhost:8080/minio/health/live
+  if [ "$?" -eq 0 ]; then
+    break
+  fi
+
+  RETRIES=$((RETRIES-1))
+  if [ "${RETRIES}" -le 0 ]; then
+    echo "Stack is not ready :(" >&2
+    exit 1
+  fi
+
+  sleep 1
+done
+echo "The stack is ready! :)"
+
+print_step "Remove the stack"
+docker-compose down
+
+exit 0
